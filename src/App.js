370a905035f45a841c415f5660b2daa6d26b567a
Anne Quiedeville <anne.quiedeville@orange.fr>
2020-01-18 18:38:18 +0100

    quest 08 component did mount done

--- src/App.js
+++ src/App.js
@@ -7,26 +7,34 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: ''
+      quote: null
     };
     this.getQuote = this.getQuote.bind(this);
     }
   
-    getQuote() {
-      axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
-        .then(response => response.data)
-        .then(data => {
-          console.log(data[0]);
-          this.setState({
-            quote: data[0],
-          });
+  getQuote() {
+    axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+      .then(response => response.data)
+      .then(data => {
+        console.log(data[0]);
+        this.setState({
+          quote: data[0],
         });
-    }
+      });
+  }
+
+  componentDidMount() {
+    this.getQuote();
+  }
 
   render() {
   return (
     <div className="App">
-      <DisplayQuote quote={this.state.quote} />
+      {
+        this.state.quote 
+          ? <DisplayQuote quote={this.state.quote} />
+          : <p>Loading...</p>
+      }
       <button type="button" onClick={this.getQuote}>Get quote</button>
     </div>
     );
